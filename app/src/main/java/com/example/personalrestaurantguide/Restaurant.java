84cecbdfac03bdc06fca72e02a88fd0d7de739af
Gone-M <metcivan@gmail.com>
2024-12-06 16:48:27 -0500

    Restaurant.java Civan

--- app/src/main/java/com/example/personalrestaurantguide/Restaurant.java
+++ app/src/main/java/com/example/personalrestaurantguide/Restaurant.java
@@ -1,18 +1,24 @@
 package com.example.personalrestaurantguide;
 
 public class Restaurant {
+    private int id;
     private String name;
     private String address;
     private String phone;
     private String description;
     private int rating;
 
-    public Restaurant(String name, String address, String phone, String description, int rating) {
+    public Restaurant(int id, String name, String address, String phone, String description, int rating) {
+        this.id = id;
         this.name = name;
         this.address = address;
         this.phone = phone;
         this.description = description;
         this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
@@ -34,4 +40,5 @@ public class Restaurant {
     public int getRating() {
         return rating;
     }
+
 }
